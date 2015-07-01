dc2c142caf74294fbe9d04f81ce8c071beb30a8c
Pawel Giers <pawel.giers@gmail.com>
2015-07-01 16:32:22 +0100

    add test files

---
+++ mitaka_driver.rb
@@ -0,0 +1,24 @@
+require 'selenium-webdriver'
+require 'rspec/expectations'
+include RSpec::Matchers
+
+def setup
+  @driver = Selenium::WebDriver.for :remote, url:
+    'http://172.17.0.12:4445'
+end
+
+def teardown
+  @driver.quit
+end
+
+def run
+  setup
+  yield
+  teardown
+end
+
+run do
+  @driver.get 'http://www.google.com'
+  expect(@driver.title).to eql 'The Internet'
+  puts @driver.title
+end
